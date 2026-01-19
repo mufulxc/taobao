0eedc80b6346e2528c466b4b1b67f352fbe6357e
mufulxc <kangemao@gmail.com>
2026-01-19 19:47:55 +0800

    2026-01-19_194752

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from db import fetch_sql
+import os
 
 app = Flask(__name__) 
 
@@ -9,4 +10,5 @@ def index():
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
